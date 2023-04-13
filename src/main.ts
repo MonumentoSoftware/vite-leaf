adbaea69825f34372bb661a2bb17bad980bdb989
Pedrokpaxo <pedrograxxa@gmail.com>
2023-04-13 16:30:03 -0300

    added readme to the project

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import { Map } from "leaflet";
 import { MapCreator } from "./lib/map-toolbox";
 
 /**
- * Base class for creating a leaflet map.
+ * Base class for creating a Leaflet map.
  */
-class BaseMapApp {
-  private el!: HTMLElement;
+export class BaseMapApp {
+  private el: HTMLElement;
   private mapDiv: HTMLDivElement;
   public map!: Map;
 
+  /**
+   * Creates a new instance of `BaseMapApp`
+   * and attaches a `div` element for the map to the `#app` element in the DOM.
+   */
   constructor() {
     this.el = document.querySelector("#app") as HTMLDivElement;
     this.mapDiv = document.createElement("div");
@@ -17,10 +21,9 @@ class BaseMapApp {
   }
 
   /**
-   * Creates a leaflet map instance,
-   * and adds a tile layer to it.
+   * Sets up a Leaflet map instance and adds a default satellite tile layer to it.
    */
-  async setupMap() {
+  async setupMap(): Promise<void> {
     this.map = MapCreator.setupMap("map-div");
     const layer = await MapCreator.getDefaultSateliteLayer();
     if (layer) layer.addTo(this.map);
