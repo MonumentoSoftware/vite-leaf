56ecfa25193ea58d96aa796d6b47ca6a2a4383cc
pedrokpaxo <pedrograxxa@gmail.com>
2023-12-25 21:30:52 -0300

    ⚒️  update map mechanics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { Map } from "leaflet";
+import { Layer, LayerGroup, Map, tileLayer } from "leaflet";
 import { MapCreator } from "./lib/map-toolbox";
+import { MapTilesProviders } from "./lib/common-types";
 
 /**
  * Base class for creating a Leaflet map.
@@ -8,6 +9,8 @@ export class BaseMapApp {
   private el: HTMLElement;
   private mapDiv: HTMLDivElement;
   public map!: Map;
+  protected tileLayer = new LayerGroup();
+  selection: HTMLSelectElement;
 
   /**
    * Creates a new instance of `BaseMapApp`
@@ -17,7 +20,24 @@ export class BaseMapApp {
     this.el = document.querySelector("#app") as HTMLDivElement;
     this.mapDiv = document.createElement("div");
     this.mapDiv.setAttribute("id", "map-div");
+    this.selection = document.createElement("select");
+    this.selection.setAttribute("id", "map-selection");
+
+    const option = MapCreator.getTileMapper();
+    option.map((item) => {
+      const option = document.createElement("option");
+      option.value = item.key;
+      option.text = item.value;
+      this.selection.appendChild(option);
+    });
+    this.selection.addEventListener<"change">("change", (event) => {
+      const target = event.target as HTMLSelectElement;
+      const layer = this.getMapLayer(target.value as MapTilesProviders);
+      if (layer) this.changeMapLayer(layer);
+    });
     this.el.append(this.mapDiv);
+    this.el.append(this.selection);
+    Promise.all([this.setupMap()]);
   }
 
   /**
@@ -25,10 +45,30 @@ export class BaseMapApp {
    */
   async setupMap(): Promise<void> {
     this.map = MapCreator.setupMap("map-div");
-    const layer = await MapCreator.getDefaultSateliteLayer();
-    if (layer) layer.addTo(this.map);
+    this.selection.value = "GoogleSatellite";
+    const layer = this.getMapLayer("GoogleSatellite");
+    if (layer) {
+      layer.addTo(this.tileLayer);
+      this.tileLayer.addTo(this.map);
+    } else {
+      alert("No layer");
+    }
+  }
+
+  getMapLayer(provider: MapTilesProviders): Layer | undefined {
+    const tile = MapCreator.tileMapper[provider];
+    const attribution = "monumentosoftware.com";
+    const layer = tileLayer(tile, { attribution });
+    if (layer) {
+      return layer;
+    }
+    return undefined;
+  }
+
+  changeMapLayer(newLayer: Layer): void {
+    this.tileLayer.clearLayers();
+    newLayer.addTo(this.tileLayer);
   }
 }
 
-const app = new BaseMapApp();
-app.setupMap();
+new BaseMapApp();
